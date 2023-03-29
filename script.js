129a08da32144fdc4cd7dc0d277c545b3008f9c0
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-03-29 15:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const fn = document.querySelector("#fn");
+const ln = document.querySelector("#ln");
+const pn = document.querySelector("#pn");
+const ml = document.querySelector("#ml");
+const btn = document.querySelector("#sub");
+let f = document.getElementById('myForm').getElementsByTagName("INPUT");
+
+
+
+btn.addEventListener("click", ()=>{
+	alert(fn.name + " " +fn.value +"\n" + ln.name +" "+ln.value +"\n" + pn.name +" "+pn.value
+		 +"\n" + ml.name +" "+ml.value);
+});
